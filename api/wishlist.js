93e77c121dfe90d6e6863a082c98000a3bc58103
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2025-01-26 23:08:51 -0300

    fix: adjust delete

--- api/wishlist.js
+++ api/wishlist.js
@@ -41,7 +41,7 @@ export default async function handler(req, res) {
     }
 
     if (req.method === 'DELETE') {
-      const { productCode } = req.query;
+      const productCode = req.url.split('/').pop();
 
       if (!productCode) {
         return res.status(400).json({
